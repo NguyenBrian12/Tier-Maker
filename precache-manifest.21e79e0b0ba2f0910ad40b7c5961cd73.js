2d565742998d32a686ee8c85ad1ea40de5d5f874
Brian Nguyen <nguyenbrian12@yahoo.com>
2019-09-17 17:15:04 -0700

    Updates

---
+++ precache-manifest.21e79e0b0ba2f0910ad40b7c5961cd73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ebb943d7ce5177c741ae0d1a309e62",
+    "url": "/Tier-Maker/index.html"
+  },
+  {
+    "revision": "4425d5ca00bf00a18261",
+    "url": "/Tier-Maker/static/css/main.c4b914f3.chunk.css"
+  },
+  {
+    "revision": "d490d945eb7913ee8ab3",
+    "url": "/Tier-Maker/static/js/2.a916798e.chunk.js"
+  },
+  {
+    "revision": "4425d5ca00bf00a18261",
+    "url": "/Tier-Maker/static/js/main.44ea6173.chunk.js"
+  },
+  {
+    "revision": "950a4fe9aae04001f6f5",
+    "url": "/Tier-Maker/static/js/runtime~main.79c94e6f.js"
+  },
+  {
+    "revision": "c6065261757ea5fdb16f97ed2bcb2dcc",
+    "url": "/Tier-Maker/static/media/background.c6065261.png"
+  }
+]);
